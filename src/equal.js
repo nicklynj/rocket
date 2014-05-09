0917026f5161eca10ad173f08ed3f47adeee65ee
Nick Nicklyn <nicklynj@gmail.com>
2014-05-09 11:30:17 -0400

    equal circular references

--- src/equal.js
+++ src/equal.js
@@ -8,12 +8,78 @@ Descends recursively into Objects.
 @param {Object} a An Object to compare.
 @param {Object} b An Object to compare.
 @return {boolean} Whether the given Objects contain identical properties.
-@test {true} Objects tested with rocket.equal.
+@test {true} Objects with same properties.
 rocket.equal({'a': 'b', 'c': [0, 1]}, {'a': 'b', 'c': [0, 1]});
 @test {false} Objects tested with equal operator.
 ({'a': 'b', 'c': [0, 1]} === {'a': 'b', 'c': [0, 1]});
+@test {false} Objects without same properties.
+rocket.equal({'a': 'b'}, {'a': 'b', 'c': [0, 1]});
+@test {true} HTMLBodyElement.
+rocket.equal(document.body, document.body);
+@test {true} Equivalent HTMLDivElement.
+rocket.equal(document.createElement('div'), document.createElement('div'));
+@test {false} Equivalent HTMLDivElement but appended to another Element.
+var foo = document.createElement('div');
+var bar = document.createElement('div');
+var baz = document.createElement('div');
+baz.appendChild(foo);
+baz.appendChild(bar);
+rocket.equal(foo, bar);
+@test {false} Two different HTMLElement.
+rocket.equal(document.createElement('div'), document.createElement('span'));
+@test {true} Circular references that are equal.
+var foo = {};
+var bar = {};
+var baz = {};
+
+foo.foo = baz;
+bar.foo = baz;
+
+baz.foo = foo;
+baz.bar = bar;
+
+baz.baz = 'baz';
+
+rocket.equal(foo, bar);
+@test {false} Circular references that are not equal.
+var foo = {};
+var bar = {};
+var baz = {};
+
+foo.foo = baz;
+bar.foo = bar; // self circular reference
+
+baz.foo = foo;
+baz.bar = bar;
+
+baz.baz = 'baz';
+
+rocket.equal(foo, bar);
 */
 rocket.equal = function(a, b) {
+
+  return rocket.equal.equal_(a, b, [a], [b]);
+
+};
+
+
+/**
+Equal function with reference Array.
+
+@private
+@param {Object} a
+@param {Object} b
+@param {Array.<Object>} as
+@param {Array.<Object>} bs
+@return {boolean}
+*/
+rocket.equal.equal_ = function(a, b, as, bs) {
+
+  for (var i = 0; as[i]; ++i) {
+    if ((as[i] === a) && (bs[i] === b)) {
+      return true;
+    }
+  }
 
   if (
       a === null || typeof a !== 'object' ||
@@ -34,10 +100,15 @@ rocket.equal = function(a, b) {
       return false;
     }
 
+    as.push(a);
+    bs.push(b);
+
     for (var i in a) {
-      if (!rocket.equal(a[i], b[i])) {
+
+      if (!rocket.equal.equal_(a[i], b[i], as, bs)) {
         return false;
       }
+
     }
 
     for (var i in b) {
