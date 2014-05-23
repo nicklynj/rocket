b290472151c034cf991800963867799217dcfa7b
Nick Nicklyn <nicklynj@gmail.com>
2014-05-23 16:28:25 -0400

    dependency management

--- compile.cmd
+++ compile.cmd
@@ -4,113 +4,22 @@ cls
 if not exist build mkdir build
 if not exist doc mkdir doc
 
-
-echo.
-echo.
-echo.
-echo.
-echo documenting...
+echo. && echo. && echo. && echo. && echo documenting...
 php -f doc/hermes.php
 
-echo.
-echo.
-echo.
-echo.
-echo generating development...
+echo. && echo. && echo. && echo. && echo generating manifest...
+php -f manifest.php
 
-type src\rocket.js                                 > build\rocket.development.js
+echo. && echo. && echo. && echo. && echo generating development...
 
-type src\inherits.js                              >> build\rocket.development.js
-type src\event.js                                 >> build\rocket.development.js
-type src\eventlistener.js                         >> build\rocket.development.js
-type src\eventtarget.js                           >> build\rocket.development.js
-type src\version.js                               >> build\rocket.development.js
-type src\expando.js                               >> build\rocket.development.js
-type src\guid.js                                  >> build\rocket.development.js
-type src\elements.js                              >> build\rocket.development.js
-type src\xmlhttprequest.js                        >> build\rocket.development.js
-type src\disposable.js                            >> build\rocket.development.js
-type src\component.js                             >> build\rocket.development.js
-type src\input.js                                 >> build\rocket.development.js
-type src\autoselect.js                            >> build\rocket.development.js
-type src\autosuggest.js                           >> build\rocket.development.js
-type src\autotab.js                               >> build\rocket.development.js
-type src\dateinput.js                             >> build\rocket.development.js
-type src\timeinput.js                             >> build\rocket.development.js
+if exist build\rocket.development.js del build\rocket.development.js
+for /F "tokens=*" %%A in (build\manifest) do type %%A >> build\rocket.development.js
 
-type src\$.js                                     >> build\rocket.development.js
-type src\arrayify.js                              >> build\rocket.development.js
-type src\bind.js                                  >> build\rocket.development.js
-type src\chunk.js                                 >> build\rocket.development.js
-type src\clamp.js                                 >> build\rocket.development.js
-type src\clone.js                                 >> build\rocket.development.js
-type src\construct.js                             >> build\rocket.development.js
-type src\cookie.js                                >> build\rocket.development.js
-type src\createelement.js                         >> build\rocket.development.js
-type src\dateisostring.js                         >> build\rocket.development.js
-type src\debounce.js                              >> build\rocket.development.js
-type src\equal.js                                 >> build\rocket.development.js
-type src\every.js                                 >> build\rocket.development.js
-type src\extend.js                                >> build\rocket.development.js
-type src\filter.js                                >> build\rocket.development.js
-type src\flip.js                                  >> build\rocket.development.js
-type src\foreach.js                               >> build\rocket.development.js
-type src\htmlentities.js                          >> build\rocket.development.js
-type src\indexof.js                               >> build\rocket.development.js
-type src\insertrule.js                            >> build\rocket.development.js
-type src\isarray.js                               >> build\rocket.development.js
-type src\isempty.js                               >> build\rocket.development.js
-type src\json.js                                  >> build\rocket.development.js
-type src\key.js                                   >> build\rocket.development.js
-type src\keys.js                                  >> build\rocket.development.js
-type src\lastindexof.js                           >> build\rocket.development.js
-type src\lexeme.js                                >> build\rocket.development.js
-type src\load.js                                  >> build\rocket.development.js
-type src\map.js                                   >> build\rocket.development.js
-type src\now.js                                   >> build\rocket.development.js
-type src\numberformat.js                          >> build\rocket.development.js
-type src\object.js                          	  >> build\rocket.development.js
-type src\padleft.js                               >> build\rocket.development.js
-type src\padright.js                              >> build\rocket.development.js
-type src\pluck.js                                 >> build\rocket.development.js
-type src\queryselectorall.js                      >> build\rocket.development.js
-type src\random.js                                >> build\rocket.development.js
-type src\range.js                                 >> build\rocket.development.js
-type src\ready.js                                 >> build\rocket.development.js
-type src\reduce.js                                >> build\rocket.development.js
-type src\reduceright.js                           >> build\rocket.development.js
-type src\setinterval.js                           >> build\rocket.development.js
-type src\settimeout.js                            >> build\rocket.development.js
-type src\some.js                                  >> build\rocket.development.js
-type src\sort.js                                  >> build\rocket.development.js
-type src\step.js                                  >> build\rocket.development.js
-type src\strtodate.js                             >> build\rocket.development.js
-type src\strtotime.js                             >> build\rocket.development.js
-type src\sum.js                                   >> build\rocket.development.js
-type src\table.js                                 >> build\rocket.development.js
-type src\throttle.js                              >> build\rocket.development.js
-type src\trim.js                                  >> build\rocket.development.js
-type src\trimleft.js                              >> build\rocket.development.js
-type src\trimright.js                             >> build\rocket.development.js
-type src\unique.js                                >> build\rocket.development.js
-type src\values.js                                >> build\rocket.development.js
-
-
-
-echo.
-echo.
-echo.
-echo.
-echo linting...
+echo. && echo. && echo. && echo. && echo linting...
 c:\python\scripts\gjslint.exe --strict --time --custom_jsdoc_tags=namespace,example,name,ignore,version,memberof,test -r src
 
-
-echo.
-echo.
-echo.
-echo.
-echo compiling, build and simpling...
-java -jar C:\git\closure-compiler\build\compiler.jar  --compilation_level SIMPLE_OPTIMIZATIONS --use_types_for_optimization --externs src\externs.js --js_output_file build\rocket.js ^
+echo. && echo. && echo. && echo. && echo compiling...
+java -jar C:\git\closure-compiler\build\compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --use_types_for_optimization --externs src\externs.js --js_output_file build\rocket.js ^
  ^
 --js src\rocket.js ^
  ^
@@ -190,11 +99,7 @@ java -jar C:\git\closure-compiler\build\compiler.jar  --compilation_level SIMPLE
 --js src\values.js
 
 
-echo.
-echo.
-echo.
-echo.
-echo checking and advancing...
+echo. && echo. && echo. && echo. && echo advanced checking...
 java -jar C:\git\closure-compiler\build\compiler.jar --warning_level VERBOSE --compilation_level ADVANCED_OPTIMIZATIONS --externs src\externs.js --summary_detail_level 3 --formatting PRETTY_PRINT --manage_closure_dependencies --jscomp_warning accessControls --jscomp_warning ambiguousFunctionDecl --jscomp_warning checkEventfulObjectDisposal --jscomp_warning checkRegExp --jscomp_warning checkStructDictInheritance --jscomp_warning checkTypes --jscomp_warning checkVars --jscomp_warning const --jscomp_warning constantProperty --jscomp_warning deprecated --jscomp_warning duplicateMessage --jscomp_warning es3 --jscomp_warning es5Strict --jscomp_warning externsValidation --jscomp_warning fileoverviewTags --jscomp_warning globalThis --jscomp_warning internetExplorerChecks --jscomp_warning invalidCasts --jscomp_warning misplacedTypeAnnotation --jscomp_warning missingProperties --jscomp_warning missingProvide --jscomp_warning missingRequire --jscomp_warning missingReturn --jscomp_warning nonStandardJsDocs --jscomp_warning reportUnknownTypes --jscomp_warning suspiciousCode --jscomp_warning strictModuleDepCheck --jscomp_warning typeInvalidation --jscomp_warning undefinedNames --jscomp_warning undefinedVars --jscomp_warning unknownDefines --jscomp_warning uselessCode --use_types_for_optimization --jscomp_warning visibility --extra_annotation_name test ^
  ^
 --js src\rocket.js ^
---
+++ manifest.php
@@ -0,0 +1,98 @@
+<?php
+
+new manifest('src');
+
+class manifest {
+
+	function __construct($file_root) {
+
+		$data = array();
+
+    $var = '(?:[\w\$][\d\w\$]*)';
+
+		foreach (scandir($file_root) as $file_name) {
+
+			if (in_array($file_name, array('.', '..', 'externs.js'))) {
+				continue;
+			}
+
+      $contents = file_get_contents($file_root . DIRECTORY_SEPARATOR . $file_name);
+
+      $contents = preg_replace('/\/\*\*.+?\*\//s', '', $contents);
+
+      $dep = array(
+        'file_path' => $file_root . DIRECTORY_SEPARATOR . $file_name,
+        'provides' => array(),
+        'requires' => array(),
+      );
+
+      preg_match_all('/^(?:var )?('.$var.'(?:\.'.$var.')*)\s+=/m', $contents, $matches);
+
+      foreach ($matches[1] as $match) {
+
+        $dep['provides'][$match] = true;
+
+        $match = explode('.', $match);
+        while(count($match)) {
+          array_pop($match);
+          if (end($match) === 'prototype') {
+            array_pop($match);
+          }
+          $req = implode('.', $match);
+          if ($req) {
+            $dep['requires'][$req] = true;
+          }
+        }
+      }
+
+      foreach ($dep['requires'] as $req => $foo) {
+        if (isset($dep['provides'][$req])) {
+          unset($dep['requires'][$req]);
+        }
+      }
+
+      preg_match_all('/^('.$var.'(\.'.$var.')*)\(\s*(.+?)\s*\);/sm', $contents, $matches);
+
+
+      foreach ($matches[1] as $match) {
+        $dep['requires'][$match] = true;
+      }
+
+      foreach ($matches[3] as $match) {
+        foreach (preg_split('/\s*,\s*/', $match) as $req) {
+          $dep['requires'][$req] = true;
+        }
+      }
+
+      $data[] = $dep;
+
+    }
+
+    for($i = 0; isset($data[$i]); ++$i) {
+      foreach ($data[$i]['requires'] as $req => $foo) {
+        for($j = 0; isset($data[$j]); ++$j) {
+          foreach ($data[$j]['provides'] as $prov => $foo) {
+            if ($req === $prov) {
+              if ($i < $j) {
+                array_splice($data, $j, 0, array_splice($data, $i, 1));
+                --$i;
+                continue(4);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    $file_paths = array();
+
+    for($i = 0; isset($data[$i]); ++$i) {
+      $file_paths[] = $data[$i]['file_path'];
+    }
+
+    file_put_contents('build/manifest', implode("\n", $file_paths));
+
+	}
+
+}
+
