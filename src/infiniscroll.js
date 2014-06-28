d4bbeff91b4793607e3d6d2c9ccb6a92df38163c
nicklynj <nicklynj@gmail.com>
2014-06-28 13:15:22 -0400

    InfiniScroll

---
+++ src/infiniscroll.js
@@ -0,0 +1,15 @@
+
+
+
+/**
+@constructor
+@extends {rocket.Component}
+*/
+rocket.InfiniScroll = function() {};
+rocket.inherits(rocket.Draggable, rocket.Component);
+
+
+/**
+@param {rocket.Elements} element
+*/
+rocket.InfiniScroll.prototype.decorate = function(element) {};
