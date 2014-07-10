89b83a023aa29f737996a4bd3614484b902b7b00
nicklynj <nicklynj@gmail.com>
2014-07-09 21:22:32 -0400

    mround/round

---
+++ src/mround.js
@@ -0,0 +1,27 @@
+
+
+
+/**
+Round a number to a given multiple.
+
+@param {number} x The number to round to the nearest multiple.
+@param {number} multiple The multiple to which to round x.
+@return {number} The rounded number.
+
+@test {10} Round up to ten.
+rocket.mround(5, 10);
+
+@test {10} Round down to ten.
+rocket.mround(14.9, 10);
+
+@test {0} Round up to zero.
+rocket.mround(-5, 10);
+
+@test {1.5} Round up to one and a half.
+rocket.mround(1.25, 0.5);
+*/
+rocket.mround = function(x, multiple) {
+
+  return Math.round(x / multiple) * multiple;
+
+};
---
+++ src/round.js
@@ -0,0 +1,36 @@
+
+
+
+/**
+Round a number to a given number of decimals.
+
+@param {number} x The number to round.
+@param {number} decimals The number of decimals.
+@return {number} The rounded number.
+
+@test {1235} Rounding to zero decimal places.
+rocket.round(1234.5678, 0);
+
+@test {1234.57} Rounding to two decimal places.
+rocket.round(1234.5678, 2);
+
+@test {1200} Rounding to negative two decimal places.
+rocket.round(1234.5678, -2);
+
+@test {-10} Rounding to zero decimal places.
+rocket.round(-10.5, 0);
+*/
+rocket.round = function(x, decimals) {
+
+  if (decimals) {
+
+    var pow = Math.pow(10, decimals);
+    return Math.round(x * pow) / pow;
+
+  } else {
+
+    return Math.round(x);
+
+  }
+
+};
