a0de8202119455c754b975c2a3c332fd23153799
Nick Nicklyn <nicklynj@gmail.com>
2014-10-22 17:08:11 -0400

    added expire days to cookie

--- src/cookie.js
+++ src/cookie.js
@@ -6,41 +6,53 @@ Sets/gets cookie values.
 @param {(string|Object.<string, string>)=} opt_values
   Value to get or key/values to set.
 @param {string=} opt_value Value to set.
+@param {number=} opt_days The number of days after which the cookie expires.
 @return {(Object.<string, string>|string)} The cookie.
 
 @test {'bar'} Set and get the value of foo.
 rocket.cookie('foo', 'bar');
 rocket.cookie('foo');
 
+@test {'bar'} Set and get the value of foo to expire tomorrow.
+rocket.cookie('foo', 'bar', 1);
+rocket.cookie('foo');
+
 @test {'foo'} Set and get the value of bar.
 rocket.cookie({'bar': 'foo'});
 rocket.cookie('bar');
 
-@test {'bar=foo'} Set and get the value of "foo=bar".
-rocket.cookie({'foo=bar': 'bar=foo'});
-rocket.cookie('foo=bar');
+@test {'bar=foo;'} Set and get the value of "foo=bar;".
+rocket.cookie({'foo=bar;': 'bar=foo;'});
+rocket.cookie('foo=bar;');
 
 */
-rocket.cookie = function(opt_values, opt_value) {
+rocket.cookie = function(opt_values, opt_value, opt_days) {
 
   var key_value_obj;
+  var expires = '';
 
-  if (arguments.length === 2) {
+  switch (arguments.length) {
 
-    key_value_obj = {};
-    key_value_obj[opt_values] = opt_value;
+    case 3:
+      expires = '; expires=' +
+          (new Date(rocket.now() + opt_days * 86400000)).toUTCString();
 
-  } else if (arguments.length === 1) {
+    case 2:
+      key_value_obj = {};
+      key_value_obj[opt_values] = opt_value;
+      break;
 
-    if (typeof opt_values === 'string') {
+    case 1:
 
-      return rocket.cookie.parse_cookie_()[opt_values] || '';
+      if (typeof opt_values === 'string') {
 
-    } else {
+        return rocket.cookie.parse_cookie_()[opt_values] || '';
 
-      key_value_obj = opt_values;
+      } else {
 
-    }
+        key_value_obj = opt_values;
+
+      }
 
   }
 
@@ -49,7 +61,8 @@ rocket.cookie = function(opt_values, opt_value) {
     for (var key in key_value_obj) {
       document.cookie =
           encodeURIComponent(key) + '=' +
-              encodeURIComponent(key_value_obj[key]);
+              encodeURIComponent(key_value_obj[key]) +
+                  expires;
     }
 
   }
